c98e98704f3aacac412a03c6f38b083215d57256
Ons Khiari <98515359+ons-khiari@users.noreply.github.com>
2025-12-27 14:44:45 +0100

    icon.svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,11 +24,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/icon.svg",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/icon.svg",
         media: "(prefers-color-scheme: dark)",
       },
       {
